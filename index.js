f2e80f16a2790eb6ab64997990196069ab7469ca
Ahmed Khaled <ahmed1999khaledaboamer1999@gmail.com>
2023-11-20 12:28:25 +0200

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const path= require('path')
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const compression = require('compression');
 
 dotenv.config({ path: "config.env" });
 const dbConnection = require("./config/database");
@@ -12,12 +16,25 @@ const couponRoutes=require("./routes")
 
 const ApiError = require("./utils/apiError");
 const globalError = require("./middleWares/globalError");
+const { webhookCheckout } = require('./services/orderService');
 
 // connect with db
 dbConnection();
 //  express app
 const app = express();
+// Enable other domains to access your application
+app.use(cors());
+app.options('*', cors());
 
+// compress all responses
+app.use(compression());
+
+// Checkout webhook
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  webhookCheckout
+);
 //  middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'uploads')))
@@ -28,10 +45,10 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // mount routes
-app.get('/test', (req,res) =>{
-  res.send('Hello World!');
-})
-// couponRoutes(app); 
+// app.get('/test', (req,res) =>{
+//   res.send('Hello World!');
+// })
+couponRoutes(app); 
 // create error and send it to handling error
 app.all("*", (req, res, next) => {
   // const err = new Error(`can't find this route ${req.originalUrl}`);
